9e0d210c2820ffb7ce02da766d49d5011b5910f0
Archaengel <god11341258@gmail.com>
2020-06-27 16:31:04 -0700

    Add app state context and provider to application

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import { AddNewItem } from './AddNewItem';
+import { AppStateProvider } from './AppStateContext';
 import { Card } from './Card';
 import { Column } from './Column';
 import { AppContainer } from './styles';
 
 function App() {
   return (
-    <AppContainer>
-      <Column text="To Do">
-        <Card text="Generate app scaffold" />
-      </Column>
-      <Column text="In Progress">
-        <Card text="Practice Prototyping" />
-      </Column>
-      <Column text="Done">
-        <Card text="Set up TS to work with React" />
-      </Column>
-      <AddNewItem toggleButtonText="+ Add another list" onAdd={console.log} />
-    </AppContainer>
+    <AppStateProvider>
+      <AppContainer>
+        <Column text="To Do">
+          <Card text="Generate app scaffold" />
+        </Column>
+        <Column text="In Progress">
+          <Card text="Practice Prototyping" />
+        </Column>
+        <Column text="Done">
+          <Card text="Set up TS to work with React" />
+        </Column>
+        <AddNewItem toggleButtonText="+ Add another list" onAdd={console.log} />
+      </AppContainer>
+    </AppStateProvider>
   );
 }
 
