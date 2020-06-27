dad01f11e656d157b1945b71a3abc627ab196c9e
Archaengel <god11341258@gmail.com>
2020-06-27 09:56:39 -0700

    feat: Add initial app components and layout
    Add styled components for Column and Card
    
    Create Card and Column components
    
    Finish initial app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
 import React from 'react';
+import { Card } from './Card';
+import { Column } from './Column';
+import { AppContainer } from './styles';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <Column text="To Do">
+        <Card text="Generate app scaffold" />
+      </Column>
+      <Column text="In Progress">
+        <Card text="Practice Prototyping" />
+      </Column>
+      <Column text="Done">
+        <Card text="Set up TS to work with React" />
+      </Column>
+    </AppContainer>
   );
 }
 
